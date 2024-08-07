0622d6802c542fd468f63febd2577be10539533e
amirthavalli1987 <amirtha.dit@gmail.com>
2024-08-07 21:00:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+//import ReactCardPlan from "./Components/ReactCardPlan";
+import ReactPricingCard from "./Components/ReactPricingCard";
+import React from "react";
+function App() {
+  return (
+    <div className="pricing py-5">
+       
+      <ReactPricingCard />
+    </div>
+  );
+}
+
+export default App;
